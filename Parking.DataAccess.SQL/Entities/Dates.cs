2b988e91c8109986400e255629e4c34793b4d6ab
Mikhail Trunev <Mikhail.Trunev@waveaccess.global>
2022-12-17 19:40:44 +0300

    add summary for model

--- Parking.DataAccess.SQL/Entities/Dates.cs
+++ Parking.DataAccess.SQL/Entities/Dates.cs
@@ -5,9 +5,13 @@ namespace Parking.DataAccess.SQL.Entities
     public class Dates
     {
         public long Id { get; set; }
-
+        /// <summary>
+        /// время приезда
+        /// </summary>
         public DateTime DateArrival { get; set; }
-
+        /// <summary>
+        /// время отъезда
+        /// </summary>
         public DateTime? DateLeaving { get; set; }
 
         public long PlaceId { get; set; }
